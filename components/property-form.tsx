c3e860e7502f414e6de6614272c53badaaa00d96
Alberto Ferreira <albertomtferreira@gmail.com>
2025-06-30 17:38:06 +0100

    35. Create the image uploader component

--- components/property-form.tsx
+++ components/property-form.tsx
@@ -9,6 +9,7 @@ import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from ".
 import { Input } from "./ui/input"
 import { Textarea } from "./ui/textarea"
 import { Button } from "./ui/button"
+import MultiImageUploader from "./multi-image-uploader"
 
 
 type Props = {
@@ -150,6 +151,7 @@ export default function PropertyForm({
             )} />
           </fieldset>
         </div>
+        <MultiImageUploader onImagesChange={() => { }} />
         <Button
           type="submit"
           className="max-w-md mx-auto mt-2 w-full flex gap-2"

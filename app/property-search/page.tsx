e9e15f16b26258165424e5b7c6da9a6a64c2a2be
Alberto Ferreira <albertomtferreira@gmail.com>
2025-07-07 21:23:16 +0100

    46. Hook up the search filters to the URL and create firestore indexes

--- app/property-search/page.tsx
+++ app/property-search/page.tsx
@@ -1,7 +1,41 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import FiltersForm from "./components/filters-form"
+import { Suspense } from "react"
+import { getProperties } from "@/data/properties";
 
-export default function PropertySearch() {
+export default async function PropertySearch({
+  searchParams
+}: {
+  searchParams: Promise<any>
+}
+) {
+  const searchParamsValues = await searchParams;
+
+  const parsedPage = parseInt(searchParamsValues?.page)
+  const parsedMinPrice = parseInt(searchParamsValues?.minPrice)
+  const parsedMaxPrice = parseInt(searchParamsValues?.maxPrice)
+  const parsedMinBedrooms = parseInt(searchParamsValues?.minBedrooms)
+
+  const page = isNaN(parsedPage) ? 1 : parsedPage;
+  const minPrice = isNaN(parsedMinPrice) ? null : parsedMinPrice;
+  const maxPrice = isNaN(parsedMaxPrice) ? null : parsedMaxPrice;
+  const minBedrooms = isNaN(parsedMinBedrooms) ? null : parsedMinBedrooms;
+
+  const properties = await getProperties({
+    pagination: {
+      page,
+      pageSize: 3
+    },
+    filter: {
+      minPrice,
+      maxPrice,
+      minBedrooms,
+      status: ["for-sale"]
+    }
+  })
+
+  console.log({ properties })
+
   return (
     <div className=" max-w-screen-lg mx-auto">
       <h1 className="text-4xl font-bold p-5">Property Search</h1>
@@ -12,7 +46,9 @@ export default function PropertySearch() {
           </CardTitle>
         </CardHeader>
         <CardContent>
-          <FiltersForm />
+          <Suspense>
+            <FiltersForm />
+          </Suspense>
         </CardContent>
 
       </Card>

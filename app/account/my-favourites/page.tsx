e19f1a8705f6d8304ffce89c14a8401383391efa
Alberto Ferreira <albertomtferreira@gmail.com>
2025-07-12 22:17:29 +0100

    60. Query for the list of favourites

--- app/account/my-favourites/page.tsx
+++ app/account/my-favourites/page.tsx
@@ -1,4 +1,23 @@
-export default function MyFavourites() {
+import { getUserFavourites } from "@/data/favourites"
+import { getPropertiesById } from "@/data/properties"
+
+export default async function MyFavourites({
+  searchParams
+}: {
+  searchParams?: Promise<any>
+}) {
+  const searchParamsValue = await searchParams
+  const page = searchParamsValue?.page ? parseInt(searchParamsValue.page) : 1
+  const pageSize = 2
+  const favourites = await getUserFavourites()
+  const allFavourites = Object.keys(favourites)
+  const totalPages = Math.ceil(allFavourites.length / pageSize)
+
+  const paginatedFavourites = allFavourites.slice((page - 1) * pageSize, page * pageSize)
+
+  const properties = await getPropertiesById(paginatedFavourites)
+  console.log({ paginatedFavourites, properties })
+
   return (
     <div>My Favourites</div>
   )
--- data/properties.ts
+++ data/properties.ts
@@ -63,5 +63,17 @@ export const getPropertyById = async (propertyId: string) => {
 
   const propertyData = { id: propertySnapshot.id, ...propertySnapshot.data() } as Property
   return propertyData
+}
 
+export const getPropertiesById = async (propertyIds: string[]) => {
+  const propertiesSnapshot = await
+    firestore
+      .collection("properties")
+      .where("__name__", "in", propertyIds)
+      .get()
+  const propertiesData = propertiesSnapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data()
+  } as Property))
+  return propertiesData
 }
